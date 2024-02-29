bbd6e07dda131147fd3f9f226e4e83af6071fc94
gilert <crankyfox@protonmail.com>
2024-02-29 10:07:53 +0100

    add prestation component and service

--- src/app/backoffice/list-prestation/list-prestation.component.ts
+++ src/app/backoffice/list-prestation/list-prestation.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrestationService } from '../../services/prestation.service';
 
 @Component({
   selector: 'app-list-prestation',
   templateUrl: './list-prestation.component.html',
   styleUrl: './list-prestation.component.css'
 })
-export class ListPrestationComponent {
+export class ListPrestationComponent implements OnInit {
+  constructor(private service: PrestationService) {}
+
+  prestations: any;
+  ngOnInit() {
+    this.refreshListPrestations();
+  }
+
+  refreshListPrestations() {
+    this.service.getAllPrestations().subscribe(
+      (response:any) => {
+        this.prestations = response;
+        console.log(this.prestations);
+      }
+    )
+  }
 
 }
